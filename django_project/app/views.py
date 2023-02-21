35490880fdd578f869da4ed6a791324b3e0d7934
Alexey Sosov <alexey.sosov@zyfra.com>
2023-02-21 19:08:57 +0300

    feat(views.py) Тестирование: 1 - Начальная версия

--- django_project/app/views.py
+++ django_project/app/views.py
@@ -1,14 +1,18 @@
-from django.utils import timezone
-
 from rest_framework.views import APIView
 from rest_framework.response import Response
 
-from . import consts, models, serializers
+from . import models, serializers
 
 
 class GetBooksView(APIView):
     def get(self, request):
-        books = models.Book.objects.select_related('author').all()
-        serializer = serializers.BookSerializer(books, many=True)
-        now = timezone.now().strftime(consts.TIME_FORMAT)
-        return Response({'ANSWER': serializer.data, 'date_time': now})
+        try:
+            book_titles = request.data['book_titles']
+        except KeyError:
+            book_qs = models.Book.objects.all()
+        else:
+            book_qs = models.Book.objects.filter(title__in=book_titles)
+
+        books_qs = book_qs.select_related('author').only('title', 'author', 'author__name')
+        serializer = serializers.BookSerializer(books_qs, many=True)
+        return Response({'ANSWER': serializer.data})
